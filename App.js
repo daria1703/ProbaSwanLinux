4db3d1fc86167e93b8e66663e2f49f43a07543b6
Daria <32822@student.pwsztar.edu.pl>
2022-03-16 21:26:59 +0100

    Nawigacja pomiędzy ekranami

--- App.js
+++ App.js
@@ -2,17 +2,37 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 import Splash from './Screens/Splash'
+import Login from './Screens/Login'
 
-const App = () => (
-  <View style={styles.container}>
-    <Splash/>
-  </View>
-);
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Splash" component={Splash} />
+        <Stack.Screen name="Login" component={Login} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
+
+// const App = () => (
+//   <View style={styles.container}>
+//     <Splash/>
+//   </View>
+// );
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//   },
   // image: {
   //   flex: 1,
   //   justifyContent: "center"
@@ -25,6 +45,6 @@ const styles = StyleSheet.create({
   //   textAlign: "center",
   //   backgroundColor: "#000000c0"
   // }
-});
+// });
 
-export default App;
+// export default App;
